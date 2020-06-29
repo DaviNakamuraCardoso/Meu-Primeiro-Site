aabe37e4154b4b59aebba8513516b19dd89365b3
Davi Nakamura Cardoso <davi.nakamura01@gmail.com>
2020-06-28 23:58:52 -0300

    Mudando o código e refatorando

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var quebrada = false
 function mudaLampada(arq) {
   if (!quebrada) {
-    document.getElementById("luz").src = "_imagens/" + arq + ".jpg";
-    if (arq == 'lampada-quebrada') {
+    document.getElementById("luz").src = "_imagens/lampada-" + arq + ".jpg";
+    if (arq == 'quebrada') {
       quebrada = true;
     }
   }
